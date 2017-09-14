8cf1817be554e5cac59069ba96ac1ff0fe388c88
abhineet27 <abhineet.sitcsc@gmail.com>
2017-09-14 23:21:22 +0530

    added flask login to get user detail

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request, abort, make_response
+from flask.ext.login import LoginManager
 from auth import auth
 from Response import Response
+import base64
 import message_validator
 
 app = Flask(__name__)
+login_manager = LoginManager()
 
 @app.route('/inbound/sms', methods=['POST'])
 @auth.login_required
+@login_manager.request_loader
 def inbound_sms():
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic ', '', 1)
+        api_key = base64.b64decode(api_key)
+        username = api_key.split(':')[0]
+        print(username)
     if not request.json:
         abort(400)
     response = message_validator.validate_message(request)
