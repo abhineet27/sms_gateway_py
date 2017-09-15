6c41ab55802dc343cb6cc05de6849f16b13b4f53
abhineet27 <abhineet.sitcsc@gmail.com>
2017-09-16 00:13:20 +0530

    minor refactoring

--- app_redis.py
+++ app_redis.py
@@ -1,10 +1,6 @@
 import redis
 
-r = redis.Redis(
-    host='localhost',
-    port=6379)
-
-
+r = redis.Redis(host='localhost', port=6379)
 
 def set(key, value, time):
         r.set(key,value,time)
