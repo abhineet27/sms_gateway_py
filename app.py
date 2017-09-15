3145d7c5ccf8fc813c07224c543f896fb4b4a4e7
abhineet27 <abhineet.sitcsc@gmail.com>
2017-09-15 23:46:15 +0530

    added outbound api

--- app.py
+++ app.py
@@ -15,16 +15,46 @@ login_manager = LoginManager()
 @auth.login_required
 @login_manager.request_loader
 def inbound_sms():
-    username = get_username_from_request(request)
-    if not request.json:
-        abort(400)
-    response = message_validator.validate_message(request,username,True)
-    if response is not None and len(response.get_error()) > 0:
-        return make_response(jsonify(response.__dict__),400)
-    if p.search(request.json['text']):
-        app_redis.set(request.json['from']+request.json['to'],"STOP", 4*60*60)
-    response = Response("inbound sms ok","")
-    return make_response(jsonify(response.__dict__),200)
+    try:
+        username = get_username_from_request(request)
+        if not request.json:
+            abort(400)
+        response = message_validator.validate_message(request,username,True)
+        if response is not None and len(response.get_error()) > 0:
+            return make_response(jsonify(response.__dict__),400)
+        if p.search(request.json['text']):
+            app_redis.set(request.json['from']+request.json['to'],"STOP", 4*60*60)
+        response = Response("inbound sms ok","")
+        return make_response(jsonify(response.__dict__),200)
+    except:
+        response = Response("","unknown failure")
+        return make_response(jsonify(response.__dict__),500)
+
+@app.route('/outbound/sms', methods=['POST'])
+@auth.login_required
+@login_manager.request_loader
+def outbound_sms():
+    try:
+        username = get_username_from_request(request)
+        if not request.json:
+            abort(400)
+        response = message_validator.validate_message(request,username,False)
+        if response is not None and len(response.get_error()) > 0:
+            return make_response(jsonify(response.__dict__),400)
+        if app_redis.get(request.json['to']+request.json['from']):
+            response = Response("sms from "+request.json['from']+" to "+request.json['to']+" blocked by STOP request","")
+            return make_response(jsonify(response.__dict__),400)
+        if None == app_redis.get(request.json['from']) or int(app_redis.get(request.json['from'])) < 50:
+            app_redis.update_cache(request.json['from'])
+        else:
+            response = Response("limit reached for from "+request.json['from'],"")
+            return make_response(jsonify(response.__dict__),400)
+        response = Response("outbound sms ok","")
+        return make_response(jsonify(response.__dict__),200)
+    except Exception as e:
+        print e
+        response = Response("","unknown failure")
+        return make_response(jsonify(response.__dict__),500)
 
 def get_username_from_request(request):
     api_key = request.headers.get('Authorization')
--- app_redis.py
+++ app_redis.py
@@ -8,3 +8,12 @@ r = redis.Redis(
 
 def set(key, value, time):
         r.set(key,value,time)
+
+def get(key):
+    return r.get(key)
+
+def update_cache(key):
+    if None == r.get(key):
+        r.set(key,1,24*60*60)
+    else:
+        r.incr(key,amount=1)
